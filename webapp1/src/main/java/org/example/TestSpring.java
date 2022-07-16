d606f92d0f4acca3e62467191dbf69b200dded0c
Trilonka <aniskov.stepan@mail.ru>
2022-07-16 18:57:42 +0300

    lesson 6 changes

--- webapp1/src/main/java/org/example/TestSpring.java
+++ webapp1/src/main/java/org/example/TestSpring.java
@@ -8,20 +8,28 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer==secondMusicPlayer;
+//
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
-        boolean comparison = firstMusicPlayer==secondMusicPlayer;
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean1", ClassicalMusic.class);
+        System.out.println(classicalMusic1.getSong());
 
-        System.out.println(comparison);
+        ClassicalMusic classicalMusic2 = context.getBean("musicBean1", ClassicalMusic.class);
+        System.out.println(classicalMusic2.getSong());
 
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(10);
-
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        System.out.println(classicalMusic1==classicalMusic2);
 
         context.close();
     }
