560a2a744b6cfed0b4701318d1dcef96bac5f0b6
Trilonka <aniskov.stepan@mail.ru>
2022-07-16 20:07:33 +0300

    lesson 9 changes

--- webapp2/src/main/java/org/example/MusicPlayer.java
+++ webapp2/src/main/java/org/example/MusicPlayer.java
@@ -1,23 +1,24 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
 
-    private Music music;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
+    public MusicPlayer(@Qualifier("someRockMusic") Music music1,
+                       @Qualifier("someClassicalMusic") Music music2)
+    {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing: " + music.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
 }
