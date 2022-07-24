65cf0108a4ec0bca70ba80859b753dc73b618c86
Trilonka <aniskov.stepan@mail.ru>
2022-07-24 17:30:30 +0300

    validator update

--- mvcapp2/src/main/java/org/example/dao/PersonDAO.java
+++ mvcapp2/src/main/java/org/example/dao/PersonDAO.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
@@ -24,6 +25,14 @@ public class PersonDAO {
 
     public List<Person> index() {
         return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query(
+                "select * from person where email=?",
+                new BeanPropertyRowMapper<>(Person.class),
+                email)
+                .stream().findAny();
     }
 
     public Person show(int id) {
